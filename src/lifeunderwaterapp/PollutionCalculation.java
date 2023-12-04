c439a75dbfd4e793f2274e5c618ea8c86eb8c402
Gdecessus <decessusoficial@gmail.com>
2023-12-04 22:35:32 +0000

    Initial commit

---
+++ src/lifeunderwaterapp/PollutionCalculation.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lifeunderwaterapp;
+
+/**
+ *
+ * @author CamaraAdmin
+ */
+public class PollutionCalculation {
+    public double calculatePollution(double minutes) {
+        return minutes * 22.83;
+    }
+}
